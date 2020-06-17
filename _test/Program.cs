b978a7206dddab02f97851a0687c7cafe2a570c3
Zev Spitz <shivisi@hotmail.com>
2020-06-17 18:29:31 +0300

    Main view complete

--- _test/Program.cs
+++ _test/Program.cs
@@ -6,7 +6,7 @@ namespace _test {
     class Program {
         [STAThread]
         static void Main(string[] args) {
-            var dte = new DateTime(2001, 1, 1, 1, 1, 1, DateTimeKind.Unspecified);
+            var dte = new DateTime(2001, 1, 1, 1, 1, 1, DateTimeKind.Utc);
             var visualizerHost = new Microsoft.VisualStudio.DebuggerVisualizers.VisualizerDevelopmentHost(dte, typeof(Visualizer), typeof(VisualizerObjectSource));
             visualizerHost.ShowVisualizer();
             Console.ReadKey(true);
