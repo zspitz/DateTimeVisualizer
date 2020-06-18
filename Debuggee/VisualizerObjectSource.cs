1c33b015512814ac95c6e7ed40ab06ac2c83fcc2
Zev Spitz <shivisi@hotmail.com>
2020-06-18 23:52:52 +0300

    CI fixes

--- Debuggee/VisualizerObjectSource.cs
+++ Debuggee/VisualizerObjectSource.cs
@@ -4,6 +4,7 @@ using System;
 
 namespace DateTimeVisualizer.Debuggee {
     public class VisualizerObjectSource : VisualizerObjectSourceBase<DateTime, Config> {
+        static VisualizerObjectSource() => SubfolderAssemblyResolver.Hook("DateTimeVisualizer");
         public override object GetResponse(DateTime target, Config config) => new Response(target, config);
     }
 }
---
+++ _testNoRef/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace _testNoRef {
+    class Program {
+        static void Main(string[] args) {
+            var dte = new DateTime(2001, 1, 1, 1, 1, 1, DateTimeKind.Unspecified);
+            Console.ReadKey(true);
+        }
+    }
+}
